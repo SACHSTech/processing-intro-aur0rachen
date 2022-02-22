3d90165e975b137e29f40ffb7ae62d4c6f231d58
Aurora Chen <99832432+aur0rachen@users.noreply.github.com>
2022-02-22 18:56:41 +0000

    flower

--- Sketch.java
+++ Sketch.java
@@ -13,10 +13,11 @@ public class Sketch extends PApplet {
 
   /** 
    * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
+   * values here i.e background, fill, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    //light blue background
+    background(158, 221, 255);
   }
 
   /**
@@ -24,12 +25,78 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    //top petal
+    stroke(240, 89, 229);
+    fill(255, 201, 251);
+    ellipse (200, 223, 35, 35);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //top right petal
+    ellipse(225, 245, 35, 35); 
+
+    //top left petal
+    ellipse(175, 245, 35, 35); 
+
+    //stem
+    stroke(1, 71, 14);
+    fill(14, 110, 32);
+    rect(199, 280, 2, 90, 1);
+
+    //bottom right petal
+    stroke(240, 89, 229);
+    fill(255, 201, 251);
+    ellipse(215, 275, 35, 35); 
+
+    //bottom left petal
+    ellipse(185, 275, 35, 35);
+
+    //center of flower
+    stroke(255, 202, 25);
+    fill(255, 236, 173);
+    ellipse(200, 250, 30, 30);
+
+    //leaf
+    line(199, 315, 159, 315);
+    
+    stroke(1, 71, 14);
+    fill(14, 110, 32);
+    beginShape();
+    curveVertex(199, 315);
+    curveVertex(199, 315);
+    curveVertex(180, 308);
+    curveVertex(159, 315);
+    curveVertex(159, 315);
+    endShape();
+
+    beginShape();
+    curveVertex(199, 315);
+    curveVertex(199, 315);
+    curveVertex(180, 322);
+    curveVertex(159, 315);
+    curveVertex(159, 315);
+    endShape();
+
+    //grass
+    fill(210, 255, 173);
+    rect(0, 370, 400, 30);
+
+    //sun
+    noStroke();
+    fill(255, 234, 0);
+    ellipse(400, 0, 115, 115);
+
+    //clouds
+    stroke(237, 244, 255);
+    fill(237, 244, 255);
+    ellipse(60, 120, 35, 35);
+    ellipse(90, 105, 50, 50);
+    ellipse(120, 120, 35, 35);
+    rect(60, 125, 60, 12, 5);
+
+    rect(330, 140, 70, 60, 40);
+    
+
+
+
   }
   
   // define other methods down here.
